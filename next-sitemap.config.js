51bf9654859556cf9e85361a95b014270b25bd85
nickespro130 <nickespro130@outlook.es>
2026-04-29 18:09:06 +0200

    added sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://reokiy.vercel.app', // <-- PON AQUÍ TU DOMINIO DE VERCEL
+  generateRobotsTxt: true, // Ya creaste tu robots.txt a mano, lo mantienes
+  // Opcional: puedes generar robots.txt automáticamente poniéndolo en true,
+  // pero como ya lo tienes, lo dejamos en false para que no se sobreescriba.
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+}
